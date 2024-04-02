bf3e6915c52036d456fe29e48c32d38ed3bbf746
[200~SaraAbedelsaid <sarausree467@gmail.com>
2024-04-01 23:39:56 -0700

    first commit

---
+++ main .js
@@ -0,0 +1,65 @@
+/*creat afunction to calculate average to group of numbers (at least 10 numbers)*/
+var x = 1+2+3+4+5+6+7+8+9+10
+console.log(x/10)
+
+/*write a function that takes the base and hieght of a triangle  and and return its area*/
+var base = 8
+var hieght =2 
+var area = 1/2*base*hieght
+console.log(area)
+
+/*creat a function that takes the age in years and returns the age in days and print it on console*/
+var y =22
+console.log(y*365)
+
+/*creat a function takes two numbers and return thier sum*/
+var s = 4
+var w = 5
+var sum = s+w
+console.log(sum)
+
+/*creat a function that takes an array contating only numbers and return the first element*/
+var num = [ "mona","sara","yaren","ahmed","yousef","mariam","mahaa","nohaa","ganaa","hala"]
+console.log(num[0])
+
+/*creat a function show your name in html document*/
+document.getElementById("den").innerHTML = "my name is : sara";
+
+/*given four numbers, return true if numbers is  more than 350. otherwise return false the sum of both*/
+var o = 100+50+50+170
+
+if (o >= 350)
+{
+document.write(true)
+};
+
+if (o <= 350)
+{
+document.write(false)
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
